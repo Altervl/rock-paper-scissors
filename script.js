1bae866b79a49310bcf1c9e32bb3c372e99aca5f
Altervl <62514019+Altervl@users.noreply.github.com>
2024-06-12 15:24:03 +0300

    Implement function for playing game of 5 rounds
    Add function playGame(), move function playRound() in it, make the game loop and print results of the game.

--- script.js
+++ script.js
@@ -19,30 +19,64 @@ function getUserChoice() {
     return answer.toLowerCase();
 }
 
-// Get computer and user inputs.
-const computerChoice = getComputerChoice();
-const userChoice = getUserChoice();
+// Function for playing entire game.
+function playGame() {
+    // Function for playing one round.
+    function playRound(computer, user) {
+        if ((computer === "rock" && user === "paper") 
+        || (computer === "paper" && user === "scissors")
+        || (computer === "scissors" && user === "rock")) {
+            return "You win";
+        }
+        
+        if ((computer === "paper" && user === "rock")
+        || (computer === "rock" && user === "scissors")
+        || (computer === "scissors" && user === "paper")) {
+            return "You lose";
+        }
 
-// Function for plaing one round.
-function playRound(computer, user) {
-    if ((computer === "rock" && user === "paper") 
-     || (computer === "paper" && user === "scissors")
-     || (computer === "scissors" && user === "rock")) {
-        return "You win";
-    }
-    
-    if ((computer === "paper" && user === "rock")
-     || (computer === "rock" && user === "scissors")
-     || (computer === "scissors" && user === "paper")) {
-        return "You lose";
+        return "Draw";
     }
 
-    return "Draw";
+    // Scores.
+    let computerScore = 0;
+    let userScore = 0;
+
+    // Game loop.
+    for (i = 0; i < 5; i++) {
+        console.log(`Round ${i}`);
+
+        // Get computer and user inputs.
+        let computerChoice = getComputerChoice();
+        let userChoice = getUserChoice();
+
+        // Play one round.
+        result = playRound(computerChoice, userChoice);
+
+        // Print the result.
+        console.log(`${result}\ncomputer: ${computerChoice}, user: ${userChoice}`);
+
+        // Increment winner's score or replay the round, if result is draw.
+        if (result === "You win") {
+            userScore++;
+        } else if (result === "You lose") {
+            computerScore++;
+        } else {
+            console.log("No winner, repeat.");
+            i--;
+        }
+    }
+
+    // Print final scores of the game.
+    console.log(`Computer: ${computerScore}, You: ${userScore}`);
+
+    // Print a final message.
+    if (userScore > computerScore) {
+        console.log("You win the game. Congrats.");
+    } else {
+        console.log("Computer wins the game. Try again.");
+    }
 }
 
-result = playRound(computerChoice, userChoice);
-console.log(`${result}\ncomputer: ${computerChoice}, user: ${userChoice}`)
-
-// TODO:
-// Run for 5 rounds in total.
-// Print "<user/computer> wins tha game".
+// Play the game.
+playGame();
