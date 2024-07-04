7454320d8c50f275c1fe0d9dc8977b63ad3c1141
Altervl <62514019+Altervl@users.noreply.github.com>
2024-07-04 15:33:48 +0300

    Make GUI version
    Replace the console functionality with  a paragraph, buttons and alerts. Now the game works on event listener and some DOM manipulation.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Select elements.
+let buttons = document.querySelector("#rpc-container");
+let msg = document.querySelector("#message");
+
 // Function for getting computer choice for a round.
 function getComputerChoice() {
     let number = Math.random() * 100;
@@ -10,14 +14,8 @@ function getComputerChoice() {
     }
     else {
         return "scissors";
-    }
-}
-// Function for getting user choice for a round.
-function getUserChoice() {
-    let answer = prompt("Rock, paper or scissors?");
-    // Make the value case insensitive.
-    return answer.toLowerCase();
-}
+    };
+};
 
 // Function for playing entire game.
 function playGame() {
@@ -27,34 +25,43 @@ function playGame() {
         || (computer === "paper" && user === "scissors")
         || (computer === "scissors" && user === "rock")) {
             return "You win";
-        }
+        };
         
         if ((computer === "paper" && user === "rock")
         || (computer === "rock" && user === "scissors")
         || (computer === "scissors" && user === "paper")) {
             return "You lose";
-        }
+        };
 
         return "Draw";
-    }
+    };
 
-    // Scores.
+    // Set up the game.
     let computerScore = 0;
     let userScore = 0;
+    let round = 1;
 
     // Game loop.
-    for (i = 0; i < 5; i++) {
-        console.log(`Round ${i}`);
-
-        // Get computer and user inputs.
+    // Add buttons listener.
+    buttons.addEventListener("click", (event) => {
+        let target = event.target;
+        let userChoice = null;
         let computerChoice = getComputerChoice();
-        let userChoice = getUserChoice();
 
-        // Play one round.
-        result = playRound(computerChoice, userChoice);
+        switch (target.id) {
+            case "rock":
+                userChoice = "rock";
+                break;
+            case "paper":
+                userChoice = "paper";
+                break;
+            case "scissors":
+                userChoice = "scissors";
+                break;
+        };
 
-        // Print the result.
-        console.log(`${result}\ncomputer: ${computerChoice}, user: ${userChoice}`);
+        // Play a round.
+        let result = playRound(computerChoice, userChoice);
 
         // Increment winner's score or replay the round, if result is draw.
         if (result === "You win") {
@@ -62,21 +69,34 @@ function playGame() {
         } else if (result === "You lose") {
             computerScore++;
         } else {
-            console.log("No winner, repeat.");
-            i--;
-        }
-    }
+            msg.textContent = `Round ${round}: no winner.`;
+        };
 
-    // Print final scores of the game.
-    console.log(`Computer: ${computerScore}, You: ${userScore}`);
+        // Print round results.
+        msg.textContent = `Round ${round}: ${result} (computer - ${computerChoice}, \
+        user - ${userChoice}, score: ${computerScore} - ${userScore})`;
 
-    // Print a final message.
-    if (userScore > computerScore) {
-        console.log("You win the game. Congrats.");
-    } else {
-        console.log("Computer wins the game. Try again.");
-    }
-}
+        round++;
+        
+        // Print final scores of the game.
+        if (userScore >= 5 || computerScore >= 5) {
+            let finalScore = `Computer: ${computerScore}, You: ${userScore}.`;
+    
+            // Print a final message.
+            if (userScore > computerScore) {
+                alert(finalScore + " You win the game. Congrats.");
+            } else {
+                alert(finalScore + " Computer wins the game. Try again.");
+            };
+            
+            // Reset the game.
+            round = 1;
+            computerScore = 0;
+            userScore = 0;
+            msg.textContent = "Choose Rock, Parer or Scissors.";
+        };
+    });
+};
 
 // Play the game.
 playGame();
